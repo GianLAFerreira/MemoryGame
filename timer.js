6cb4b9d46c180c6247a5c40062c706aaf60df0b2
Gian Lucas Alves Ferreira <gianlol201@gmail.com>
2021-08-19 11:34:20 -0300

    Adicionado ao repositório
    ":tada: git init"

---
+++ timer.js
@@ -0,0 +1,27 @@
+"use strict"
+
+let hh = 0;
+
+let mm = 0;
+
+let ss = 0;
+
+var tempo = 1000; //quantos milesimos vale 1 segundo
+
+var cron;
+
+function start() {
+
+}
+
+function pause() {
+
+}
+
+function stop() {
+
+}
+
+function time() {
+    
+}
