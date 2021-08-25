86205055e4497f214278debcf00660f50fd9a707
Gian Lucas Alves Ferreira <gianlol201@gmail.com>
2021-08-25 17:39:25 -0300

    ":tada: ajustes no ranking"

--- controller/main.js
+++ controller/main.js
@@ -1,3 +1,4 @@
+"use strict"
 const cardBoard = document.querySelector("#cardboard");
 const imgs = [
     "Letra A.svg",
@@ -44,10 +45,64 @@ cardBoard.innerHTML = cardHTML + cardHTML2;
 /** Fim da Renderização HTML */
 
 
+let hh = 0;
+let mm = 0;
+let ss = 0;
+let tempo = 17; //Quantos milésimos valem 1 segundo?
+let cron;
+
+
+//Inicia o temporizador
+function start() {
+    cron = setInterval(() => { timer(); }, tempo);
+}
+start() //quando entra na pagina ele roda a função
+
+//Para o temporizador mas não limpa as variáveis
+function pause() {
+    clearInterval(cron);
+    hh = hh;
+    mm = mm;
+    ss = ss;
+}
+
+//Para o temporizador e limpa as variáveis
+function stop() {
+    clearInterval(cron);
+    hh = 0;
+    mm = 0;
+    ss = 0;
+
+    document.getElementById('counter').innerText = '00:00:00';
+}
+
+//Faz a contagem do tempo e exibição
+function timer() {
+    ss++; //Incrementa +1 na variável ss
+
+    if (ss == 59) {     //Verifica se deu 59 segundos
+        ss = 0;         //Volta os segundos para 0
+        mm++;           //Adiciona +1 na variável mm
+
+        if (mm == 59) { //Verifica se deu 59 minutos
+            mm = 0;     //Volta os minutos para 0
+            hh++;       //Adiciona +1 na variável hora
+        }
+    }
+    //Cria uma variável com o valor tratado HH:MM:SS
+    let format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
+    //Insere o valor tratado no elemento counter
+    document.getElementById('counter').innerText = format;
+    //Retorna o valor tratado
+    return format;
+}
+// ********** fim do cronometro ************* //
+
+
 const cards = document.querySelectorAll(".memory-card");
-let firstCard,
-    secondCard;
+let firstCard, secondCard;
 let lockCards = false;
+let contador = 0
 
 function flipCard() {
     if (lockCards) 
@@ -69,8 +124,8 @@ function flipCard() {
 
 function checkForMatch() {
     let isMatch = firstCard.dataset.card === secondCard.dataset.card;
-
-    ! isMatch ? unFlipCards() : resetCards(isMatch);
+    ! isMatch  ?  unFlipCards() : resetCards(isMatch);
+    console.log(contador)  
 }
 
 function unFlipCards() {
@@ -78,44 +133,75 @@ function unFlipCards() {
     setTimeout(() => {
         firstCard.classList.remove("flip");
         secondCard.classList.remove("flip");
-
         resetCards();
     }, 1000);
 }
 
 function resetCards(isMatch = false) {
     if (isMatch) {
+        contador++;
         firstCard.removeEventListener("click", flipCard);
         secondCard.removeEventListener("click", flipCard);
     }[firstCard, secondCard, lockCards] = [null, null, false];
-}
+    if (contador === 8 ){
+        pause();
+        let jogadoresRanking = JSON.parse(localStorage.getItem('players')) || 
+        {
+            jogador:   {
+                nome:       [],
+                idJogador:  [],
+            },
+            tempo:     { 
+                tempoLevado:[],
+                idTempo:    []
+            },
+        }
+ 
+       jogadoresRanking.jogador.idJogador.push(jogadoresRanking.jogador.idJogador +1)
+       jogadoresRanking.tempo.idTempo.push(jogadoresRanking.tempo.idTempo+1)
+       jogadoresRanking.jogador.nome.push(data);
+       jogadoresRanking.tempo.tempoLevado.push(timer())
 
+       console.log(jogadoresRanking)
+
+       jogadoresRanking.tempo.tempoLevado.sort() //reordena o tempo
+       jogadoresRanking.tempo.tempoLevado.reverse() // coloca o tempo inverso
+
+    
+
+        function escreveJogadores(_valorArray, indice) {
+            const divDepois = document.querySelector('.item')
+            // Após essa esse elemento o sistema ira criar a div
+            const div = document.createElement('div');
+            // cria uma div
+            div.classList.add("itemranking")
+            // Adiciona uma classe a essa div
+            div.innerText = `${(jogadoresRanking.tempo.tempoLevado[indice])}` 
+            // escreve o nome do jogadore no conteudo
+            const body = document.querySelector('.grid')
+            body.insertBefore(div, divDepois.nextElementSibling)
+        }
+        jogadoresRanking.jogador.nome.forEach(escreveJogadores)
+        localStorage.setItem('players', JSON.stringify(jogadoresRanking))
+    };
+}
 cards.forEach(card => card.addEventListener("click", flipCard));
-// -------------- fim da logica das cartas
+
+
+// ************** fim da logica das cartas ********
 
 let url_string = window.location.href;
 let url = new URL(url_string);
 let data = url.searchParams.get("minhaVariavel");
 
 // ----------- pega jogador da url
-let jogadoresRanking = JSON.parse(localStorage.getItem('players')) || []
-jogadoresRanking.push(data)
 
-function escreveJogadores(_valorArray, indice) {
-    const divDepois = document.querySelector('.itemranking')
-    // Após essa esse elemento o sistema ira criar a div
-    const div = document.createElement('div');
-    // cria uma div
-    div.classList.add("item")
-    // Adiciona uma classe a essa div
-    div.innerText = `${jogadoresRanking[indice]}`
-    // escreve o nome do jogadore no conteudo
-    const body = document.querySelector('.grid')
-    body.insertBefore(div, divDepois.nextElementSibling)
 
+function reload() {
+    window.location.href = `index.html`;
 }
-jogadoresRanking.forEach(escreveJogadores)
 
-localStorage.setItem('players', JSON.stringify(jogadoresRanking))
-//         coloca no local storage
+
+
+
 
